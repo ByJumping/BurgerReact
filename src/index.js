2762032f29ecdda778502bb66f4183d580564b52
Aleksey Pravdin <leks16@bk.ru>
2021-10-03 16:01:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './css/style.css'
+import Rrouter from './components/Rrouter';
+
+
+render(<Rrouter/>, document.querySelector('#root'))
